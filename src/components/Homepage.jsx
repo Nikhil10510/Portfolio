2bc4a0bcc9d6c66c182d624eda0b1e40edb0eb87
Nikhil10510 <nikhilkumarr101@gmail.com>
2025-06-11 17:23:06 +0530

    view

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -19,27 +19,27 @@ function Homepage() {
         initial={{ opacity: 0 }}
         whileInView={{ opacity: 1 }}
         transition={{ duration: 0.5 }}
-        viewport={{ once: true,amount: 0.6 }}
+        viewport={{ once: true,amount: 0.8 }}
         className='container mx-auto text-center'>
         <motion.h1
           initial={{ opacity: 0, y: 100 }}
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ delay: 0.5, duration: 0.5,
            }}
-           viewport={{ once: true ,amount: 0.6}}
+           viewport={{ once: true ,amount: 0.8}}
           className='text-4xl md:text-5xl flex flex-col gap-1 font-bold mb-10 mt-8'>Hello Everyone
           <motion.span
             initial={{ opacity: 0, y: 100 }}
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ delay: 0.7, duration: 0.5 }}
-            viewport={{ once: true,amount: 0.6 }}
+            viewport={{ once: true,amount: 0.8 }}
             className='text-purple-500'>It's Nikhil Kumar</motion.span>
         </motion.h1>
         <motion.p
           initial={{ opacity: 0, y: 100 }}
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ delay: 0.9, duration: 0.5 }}
-          viewport={{ once: true,amount: 0.6 }}
+          viewport={{ once: true,amount: 0.8 }}
           className='text-gray-400 text-xl mb-8'>I am ECE Undergraduate at BIT,Mesra <br />
           I am Web Developer and a Competitive Programmer</motion.p>
          {/* Resume */}
@@ -49,7 +49,7 @@ function Homepage() {
           whileInView={{ opacity: 1, y: 0, scale: 1 }}
           transition={{ delay: 1.1, duration: 0.6, type: "spring", stiffness: 80 }}
           whileTap={{ scale: 0.97 }}
-          viewport={{ once: true,amount: 0.6 }}
+          viewport={{ once: true,amount: 0.8 }}
          href=""
          target='_blank'
          className='inline-block text-white py-4 px-6 rounded-full mt-1 font-bold transition duration-300 transform
